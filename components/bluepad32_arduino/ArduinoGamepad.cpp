2846996c1aaa5c7b3ec59bfe3a4a208da12d24e2
Ricardo Quesada <ricardoquesada@gmail.com>
2021-12-15 10:20:47 -0800

    build system: update cmake/make/sdkconfig
    Updates the builds files:
     - component.mk
     - CMakeLists.txt
     - sdkconfig.defaults
    
    Uses conditionals (which don't work that well), removes the old
    sdkconfig with sdkconfig.defatuls, and other small features.

--- components/bluepad32_arduino/ArduinoGamepad.cpp
+++ components/bluepad32_arduino/ArduinoGamepad.cpp
@@ -8,40 +8,42 @@
 #include <uni_platform_arduino.h>
 
 #include "sdkconfig.h"
-#ifdef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
+#ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
+#error "Must only be compiled when using Bluepad32 Arduino platform"
+#endif // !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
 
 Gamepad::Gamepad() : _connected(false), _state() {}
 
-bool Gamepad::isConnected() const { return _connected; }
+bool Gamepad::isConnected() const {
+    return _connected;
+}
 
 void Gamepad::setPlayerLEDs(uint8_t led) const {
-  if (!isConnected()) {
-    loge("gamepad not connected");
-    return;
-  }
+    if (!isConnected()) {
+        loge("gamepad not connected");
+        return;
+    }
 
-  if (arduino_set_player_leds(_state.idx, led) == -1)
-    loge("error setting player LEDs");
+    if (arduino_set_player_leds(_state.idx, led) == -1)
+        loge("error setting player LEDs");
 }
 
 void Gamepad::setColorLED(uint8_t red, uint8_t green, uint8_t blue) const {
-  if (!isConnected()) {
-    loge("gamepad not connected");
-    return;
-  }
+    if (!isConnected()) {
+        loge("gamepad not connected");
+        return;
+    }
 
-  if (arduino_set_lightbar_color(_state.idx, red, green, blue) == -1)
-    loge("error setting lightbar color");
+    if (arduino_set_lightbar_color(_state.idx, red, green, blue) == -1)
+        loge("error setting lightbar color");
 }
 
 void Gamepad::setRumble(uint8_t force, uint8_t duration) const {
-  if (!isConnected()) {
-    loge("gamepad not connected");
-    return;
-  }
+    if (!isConnected()) {
+        loge("gamepad not connected");
+        return;
+    }
 
-  if (arduino_set_rumble(_state.idx, force, duration) == -1)
-    loge("error setting lightbar color");
+    if (arduino_set_rumble(_state.idx, force, duration) == -1)
+        loge("error setting lightbar color");
 }
-
-#endif  // CONFIG_BLUEPAD32_PLATFORM_ARDUINO

842768c5e7a83e45a926ffc69c47b5af830625f5
Ricardo Quesada <ricardoquesada@gmail.com>
2023-02-03 06:36:22 -0800

    Add comment about how to fix watchdog in main loop

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -206,5 +206,13 @@ void loop() {
                 processBalanceBoard(myController);
         }
     }
+
+    // The main loop must have some kind of "yield to lower priority task" event.
+    // Otherwise the watchdog will get triggered.
+    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+    // Detailed info here:
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+
+    // vTaskDelay(1);
     delay(150);
 }

50c14642a75bc229f01ea749bce60949485f8079
Ricardo Quesada <ricardoquesada@gmail.com>
2023-05-20 05:33:52 -0700

    from Bluepad32 upstream

--- components/bluepad32/include/uni_platform_unijoysticle_c64.h
+++ components/bluepad32/include/uni_platform_unijoysticle_c64.h
@@ -7,7 +7,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,9 +30,10 @@ limitations under the License.
 
 typedef enum {
     UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_INVALID,   // Invalid
-    UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_3BUTTONS,  // Pots are used for mouse, paddle, extra buttons
-    UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_5BUTTONS,  // Pots are used for mouse, paddle, extra buttons
+    UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_3BUTTONS,  // Pots are used for extra buttons
+    UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_5BUTTONS,  // Pots are used for extra buttons
     UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_RUMBLE,    // Pots are used to toggle rumble
+    UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_PADDLE,    // Pots are used to control paddle (experimental)
 
     UNI_PLATFORM_UNIJOYSTICLE_C64_POT_MODE_COUNT,
 } uni_platform_unijoysticle_c64_pot_mode_t;
